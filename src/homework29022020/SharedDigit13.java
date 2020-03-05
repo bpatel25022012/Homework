3f307362209d73f6821837454fd7bea71a19f751
bpatel25022012 <bpatel2502012@gmail.com>
2020-03-05 08:12:17 +0000

    extrahomework29022020

--- src/homework29022020/SharedDigit13.java
+++ src/homework29022020/SharedDigit13.java
@@ -1,9 +1,13 @@
 package homework29022020;
 
 import java.lang.String;
+
 public class SharedDigit13 {
 
+    public static void main(String[] args) {
 
+        hasSharedDigit(12, 23);
+    }
 
     public static boolean hasSharedDigit(int number1,int number2){
         if(number1 < 10 || number1 > 99 || number2 < 10 || number2 > 99)
@@ -23,7 +27,5 @@ public class SharedDigit13 {
 
     }
 
-    public static void main(String[] args) {
-        hasSharedDigit(12, 23);
-    }
+
 }
